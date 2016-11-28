e9e646629a0902c6d0986c3f28ac4a5f8b1ef771
bright.jiang <bright.jiang@infotm.com>
2016-11-28 18:03:35 +0800

    save

--- aac/aac_parser.c
+++ aac/aac_parser.c
@@ -2,15 +2,63 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include "aac_parser.h"
 #include <unistd.h>
+#include <stdint.h>
+typedef struct
+{
+    uint16_t syncword;
+    uint8_t id;
+    uint8_t layer;
+    uint8_t protection_absent;
+    uint8_t profile;
+    uint8_t sf_index;
+    uint8_t private_bit;
+    uint8_t channel_configuration;
+    uint8_t original;
+    uint8_t home;
+    uint8_t emphasis;
+    uint8_t copyright_identification_bit;
+    uint8_t copyright_identification_start;
+    uint16_t aac_frame_length;
+    uint16_t adts_buffer_fullness;
+    uint8_t no_raw_data_blocks_in_frame;
+    uint16_t crc_check;
+
+    /* control param */
+    uint8_t old_format;
+} adts_header;
 struct aac_parser_t{
 	char *buf;
-	int size;
-	int adts_number;
-	size_t *adts_offset;
+	uint32_t size;
+	uint32_t adts_number;
+	uint32_t *adts_offset_list;
+	adts_header *adts_header_list;
 };
-int aac_parser_parse_adts_header(char *buf){
+int aac_parser_adts_fixed_header(char *buf, adts_header *header){
+
+	return 0;
+}
+int aac_parser_adts_variable_header(char *buf, adts_header *header){
+
+	return 0;
+}
+int aac_parser_adts_error_check(char *buf, adts_header *header){
+
+	return 0;
+}
+int aac_parser_parse_adts_header(char *buf, adts_header *header){
 	assert(buf != NULL);
+	header->id = buf[1] & 0x0F;
+	switch(header->id){
+		case 0:
+			break;
+		case 1:
+			break;
+		default:
+			return -1;
+	}
+
 	return 0;
 }
 int aac_parser_update_adts_header_list(struct aac_parser_t *parser){
@@ -22,7 +70,7 @@ int aac_parser_update_adts_header_list(struct aac_parser_t *parser){
 		header = (unsigned char *)(parser->buf+i);
 		//printf("header ->%X %X\n", *header,*(header+i));
 		if(*header  == 0xFF && (*(header+1) & 0xF0) == 0xF0){
-			parser->adts_offset[adts_index++] = i;
+			parser->adts_offset_list[adts_index++] = i;
 			printf("find header ->%d\n", i);
 		}
 		//break;
@@ -61,8 +109,8 @@ struct aac_parser_t *aac_parser_init(char *filename){
 		printf("read aac to memory error ->%d:%d\n", file_size, result);
 	}
 	fclose(fd);
-	parser->adts_offset = malloc((file_size / 20) * sizeof(size_t));
-	if(parser->adts_offset == NULL){
+	parser->adts_offset_list = malloc((file_size / 20) * sizeof(size_t));
+	if(parser->adts_offset_list == NULL){
 		printf("malloc adts offset list error\n");
 		return NULL;
 	}
