54ba4d12ebe24ceb44da0bb0ffa9b29484d087d1
bright.jiang <bright.jiang@infotm.com>
2017-01-13 18:19:16 +0800

    save

---
+++ demux/postion.c
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2013 Stefano Sabatini
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * @file
+ * libavformat/libavcodec demuxing and muxing API example.
+ *
+ * Remux streams from one container format to another.
+ * @example remuxing.c
+ */
+
+#include <libavutil/timestamp.h>
+#include <libavformat/avformat.h>
+
+static void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt, const char *tag)
+{
+    AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
+
+    printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
+           tag,
+           av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
+           av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
+           av_ts2str(pkt->duration), av_ts2timestr(pkt->duration, time_base),
+           pkt->stream_index);
+}
+
+int main(int argc, char **argv)
+{
+    AVOutputFormat *ofmt = NULL;
+    AVFormatContext *ifmt_ctx = NULL;
+    AVPacket pkt;
+    int index ;
+    AVStream *stream = NULL;
+    int timescale;
+    uint64_t timestamp = 0;
+    const char *in_filename;
+    int ret, i;
+
+    in_filename  = argv[1];
+
+
+    av_register_all();
+
+    if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
+        fprintf(stderr, "Could not open input file '%s'", in_filename);
+        goto end;
+    }
+
+    if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+        fprintf(stderr, "Failed to retrieve input stream information");
+        goto end;
+    }
+
+    av_dump_format(ifmt_ctx, 0, in_filename, 0);
+#if 0
+    out_filename = argv[2];
+    avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, out_filename);
+    if (!ofmt_ctx) {
+        fprintf(stderr, "Could not create output context\n");
+        ret = AVERROR_UNKNOWN;
+        goto end;
+    }
+    ofmt = ofmt_ctx->oformat;
+#endif
+
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        stream = ifmt_ctx->streams[i];
+		printf("..............>%d %d\n",stream->index , stream->id);
+		continue ;
+    }
+    int counter = 0;
+    while (1) {
+		counter++;
+        AVStream *stream, *out_stream;
+
+        ret = av_read_frame(ifmt_ctx, &pkt);
+        index = pkt.stream_index;
+        stream = ifmt_ctx->streams[index];
+        timescale = stream->time_base.den / stream->time_base.num;
+        timestamp =  pkt.dts * 1000/ timescale;
+//        printf("info -> %d %d\n", index, timescale);
+		printf("frame -->%3d ts:%8lld index:%d size:%7d pts:%8lld pos:%8lld\n",\
+				counter, timestamp, pkt.stream_index,pkt.size ,pkt.pts, pkt.pos);
+        if (ret < 0)
+            break;
+        av_packet_unref(&pkt);
+    }
+
+end:
+
+    avformat_close_input(&ifmt_ctx);
+
+
+    return 0;
+}
