e2faf2b801abbe535c326dab829d0bb5fa6fd1d7
bright.jiang <bright.jiang@infotm.com>
2017-01-13 18:30:16 +0800

    save

--- demux/postion.c
+++ demux/postion.c
@@ -31,21 +31,9 @@
 #include <libavutil/timestamp.h>
 #include <libavformat/avformat.h>
 
-static void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt, const char *tag)
-{
-    AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
-
-    printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
-           tag,
-           av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
-           av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
-           av_ts2str(pkt->duration), av_ts2timestr(pkt->duration, time_base),
-           pkt->stream_index);
-}
 
 int main(int argc, char **argv)
 {
-    AVOutputFormat *ofmt = NULL;
     AVFormatContext *ifmt_ctx = NULL;
     AVPacket pkt;
     int index ;
@@ -54,9 +42,24 @@ int main(int argc, char **argv)
     uint64_t timestamp = 0;
     const char *in_filename;
     int ret, i;
+    int index_filter = -1;
+    if(argc < 2){
+        printf("ussge %s file [index]\n", argv[0]);
+    }
 
     in_filename  = argv[1];
-
+    if(argc == 2){
+        printf("display all stream\n");
+        index_filter = -1;
+    }
+    else if(argc == 3){
+        index_filter = atoi(argv[2]);
+        if(index_filter <0  || index_filter > 4){
+            printf("input stream index error\n");
+            exit(0);
+        }
+        printf("set stream filter to ->%d\n", index_filter);
+    }
 
     av_register_all();
 
@@ -98,8 +101,14 @@ int main(int argc, char **argv)
         timescale = stream->time_base.den / stream->time_base.num;
         timestamp =  pkt.dts * 1000/ timescale;
 //        printf("info -> %d %d\n", index, timescale);
-		printf("frame -->%3d ts:%8lld index:%d size:%7d pts:%8lld pos:%8lld\n",\
-				counter, timestamp, pkt.stream_index,pkt.size ,pkt.pts, pkt.pos);
+        if(index_filter == -1){
+            printf("frame -->%3d ts:%8lld index:%d size:%7d pts:%8lld pos:%8lld\n",\
+                    counter, timestamp, pkt.stream_index, pkt.size ,pkt.pts, pkt.pos);
+        }
+        else if(index_filter = pkt.stream_index){
+            printf("frame -->%3d ts:%8lld index:%d size:%7d pts:%8lld pos:%8lld\n",\
+                    counter, timestamp, pkt.stream_index, pkt.size ,pkt.pts, pkt.pos);
+        }
         if (ret < 0)
             break;
         av_packet_unref(&pkt);
